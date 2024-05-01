5a5d27bf30a950247c56a1437c871ca09ea222b9
LordMashh <mayankkush0842@gmail.com>
2024-05-01 22:44:29 +0530

    Update App.jsx

--- Ai Recipie generator/src/App.jsx
+++ Ai Recipie generator/src/App.jsx
@@ -8,7 +8,7 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  let api = "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"
+
   const openai = new OpenAI({
     apiKey: api,
     dangerouslyAllowBrowser: true ,
