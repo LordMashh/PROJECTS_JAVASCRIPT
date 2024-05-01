adf983c2d60da954536593c046ae8052d89cc329
LordMashh <mayankkush0842@gmail.com>
2024-05-01 22:45:30 +0530

    new

--- Ai Recipie generator/src/App.jsx
+++ Ai Recipie generator/src/App.jsx
@@ -1,53 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import OpenAI from "openai";
-import './App.css'
-// import "dotenv/config";
-// import "dotenv"
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import OpenAI from "openai";
+// import './App.css'
+// // import "dotenv/config";
+// // import "dotenv"
 
-function App() {
-  const [count, setCount] = useState(0)
+// function App() {
+//   const [count, setCount] = useState(0)
 
-  const openai = new OpenAI({
-    apiKey: api,
-    dangerouslyAllowBrowser: true ,
-  });
+//   const openai = new OpenAI({
+//     apiKey: api,
+//     dangerouslyAllowBrowser: true ,
+//   });
   
-  async function main() {
-    // const completion = await openai.chat.completions.create({
-    //   messages: [{"role": "system", "content": "You are a helpful assistant."},
-    //       {"role": "user", "content": "Who won the world series in 2020?"},
-    //       {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-    //       {"role": "user", "content": "Where was it played?"}],
-    //   model: "gpt-3.5-turbo",
-    const completion = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo",
-      messages: [
-        {
-          "role": "user",
-          "content": "generate a receipe of palak panner for 5 serves"
-        }
-      ],
-      temperature: 1,
-      max_tokens: 256,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0,
-    });
-    console.log(completion);
+//   async function main() {
+//     // const completion = await openai.chat.completions.create({
+//     //   messages: [{"role": "system", "content": "You are a helpful assistant."},
+//     //       {"role": "user", "content": "Who won the world series in 2020?"},
+//     //       {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+//     //       {"role": "user", "content": "Where was it played?"}],
+//     //   model: "gpt-3.5-turbo",
+//     const completion = await openai.chat.completions.create({
+//       model: "gpt-3.5-turbo",
+//       messages: [
+//         {
+//           "role": "user",
+//           "content": "generate a receipe of palak panner for 5 serves"
+//         }
+//       ],
+//       temperature: 1,
+//       max_tokens: 256,
+//       top_p: 1,
+//       frequency_penalty: 0,
+//       presence_penalty: 0,
+//     });
+//     console.log(completion);
   
-    console.log(completion.choices[0]);
-  }
-  main();
-  return (
-    <>
-Mayank
+//     console.log(completion.choices[0]);
+//   }
+//   main();
+//   return (
+//     <>
+// Mayank
 
 
 
-    </>
-  )
-}
+//     </>
+//   )
+// }
 
-export default App
+// export default App
